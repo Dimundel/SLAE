5b20e026289bcb983d1cdc47a891e7890887d233
Dmitrii <deenixie@gmail.com>
2023-02-18 19:34:34 +0300

    minor changes in tridiagonal_test.cpp

--- tests/tridiagonal_test.cpp
+++ tests/tridiagonal_test.cpp
@@ -12,17 +12,14 @@ const TridiagonalMatrix slae2({{0, 2, 3}, {6, 3, 9}, {2, 5, 2}, {4, 3, 0}});
 const std::vector<float> column2{21, 69, 34, 22};
 const std::vector<float> solution2{3, 5, 4, 2};
 
-TEST(TridiagonalTest, CheckSolution1) {
-    std::vector<float> res = solve(slae1, column1);
-    for (int i = 0; i < res.size(); ++i) {
-        EXPECT_NEAR(res[i], solution1[i], ERROR);
+TEST(TridiagonalTest, CheckSolving) {
+    std::vector<float> res1 = solve(slae1, column1);
+    for (int i = 0; i < res1.size(); ++i) {
+        EXPECT_NEAR(res1[i], solution1[i], ERROR);
     }
-}
-
-TEST(TridiagonalTest, CheckSolution2) {
-    std::vector<float> res = solve(slae2, column2);
-    for (int i = 0; i < res.size(); ++i) {
-        EXPECT_NEAR(res[i], solution2[i], ERROR);
+    std::vector<float> res2 = solve(slae2, column2);
+    for (int i = 0; i < res2.size(); ++i) {
+        EXPECT_NEAR(res2[i], solution2[i], ERROR);
     }
 }
 
