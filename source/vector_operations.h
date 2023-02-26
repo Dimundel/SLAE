f3a161b0d377a0ab5958f8915cc4b18c2bd9665e
Dmitrii <deenixie@gmail.com>
2023-02-26 16:12:15 +0300

    add header guards in vector_operations.h

--- source/vector_operations.h
+++ source/vector_operations.h
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <vector>
 
+#ifndef VECTOR_OPERATIONS_H
+#define VECTOR_OPERATIONS_H
 std::vector<float> &operator+=(std::vector<float> &vec1,
                                const std::vector<float> &vec2);
 
@@ -20,3 +22,5 @@ std::vector<float> operator*(const std::vector<float> &vec, const int num);
 std::vector<float> operator*(const int num, const std::vector<float> &vec);
 
 int dot_product(const std::vector<float> vec1, const std::vector<float> vec2);
+
+#endif
